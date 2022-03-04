a3a80991e4fd7602eb589fa4b514d53533ebe918
ramonfbarbosa <ramonmfb777@gmail.com>
2022-03-04 17:26:47 -0300

    Database connection

---
+++ MovieAPI/Data/MovieContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieAPI.Models;
+
+namespace MovieAPI.Data
+{
+    public class MovieContext : DbContext 
+    { 
+        
+        public MovieContext(DbContextOptions<MovieContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
--- MovieAPI/Startup.cs
+++ MovieAPI/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MovieAPI.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,7 +28,10 @@ namespace MovieAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            var connectionString = Configuration["DbContextSettings:ConnectionString"];
+            services.AddDbContext<MovieContext>(options =>
+            options.UseNpgsql(connectionString)
+             );
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
