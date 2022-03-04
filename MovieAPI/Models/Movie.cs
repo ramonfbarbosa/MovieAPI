1642bc9875a252433c56d5826c12df1d2aca424b
ramonfbarbosa <ramonmfb777@gmail.com>
2022-03-04 15:07:09 -0300

    validating parameters

--- MovieAPI/Models/Movie.cs
+++ MovieAPI/Models/Movie.cs
@@ -1,10 +1,16 @@
-﻿namespace MovieAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieAPI.Models
 {
     public class Movie
     {
+        [Required(ErrorMessage = "You need to fill the field")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "You need to fill the field")]
         public string Director { get; set; }
+        [StringLength(30, ErrorMessage = "The field cannot exceed 30 characters")]
         public string Genre { get; set; }
+        [Range(1, 600, ErrorMessage = "The duration must have a minimum of 1 and a maximum of 600 minutes")]
         public int Duration { get; set; }
 
         public Movie()
