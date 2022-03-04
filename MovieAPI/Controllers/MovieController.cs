6607d90a003b6ac9ceb785f655ae22816d1e29bc
ramonfbarbosa <ramonmfb777@gmail.com>
2022-03-04 15:00:27 -0300

    getting data from a movie

---
+++ MovieAPI/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieAPI.Models;
+using System.Collections.Generic;
+
+namespace MovieAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController
+    {
+        private static List<Movie> movies = new List<Movie>();
+
+        [HttpPost]
+        public void AddMovie([FromBody] Movie movie)
+        {
+            movies.Add(movie);
+            System.Console.WriteLine(movie.Title + " Added");
+        }
+    }
+}
---
+++ MovieAPI/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿namespace MovieAPI.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+
+        public Movie()
+        {
+        }
+        public Movie(string title, string director, string genre, int duration)
+        {
+            Title = title;
+            Director = director;
+            Genre = genre;
+            Duration = duration;
+        }
+    }
+}
